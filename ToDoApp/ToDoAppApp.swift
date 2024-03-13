7ece3c1d0b39de85c6b6d08beb80a2ae3447005b
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2024-03-13 20:26:03 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Tariq Almazyad on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
