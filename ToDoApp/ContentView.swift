4241c8417f7ecd502e66056ec3a99beb3777e46c
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2024-03-13 21:37:22 +0300

    Add ability to add tasks
    we added ability for the user to add tasks in the app + AddTask will dismiss auto after user add the task to the tasks array .

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -8,30 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    // step 1 : create var to show/hide
+    @State var isShowingAddNewTaskView: Bool = false
+    @State var tasks: [String] = []
     var body: some View {
         NavigationStack{
             List {
-                ForEach(0 ..< 5) { item in
-                    Text("123")
-                        .swipeActions{
-                            Button(action: {
-                                
-                            }, label: {
-                                Image(systemName: "trash")
-                                    .tint(.red)
-                            })
-                        }
+                ForEach(tasks, id:\.self) { task in
+                    NavigationLink {
+                        Text(task)
+                    } label: {
+                        Text(task)
+                    }
+
+//                        .swipeActions{
+//                            Button(action: {
+//                                
+//                            }, label: {
+//                                Image(systemName: "trash")
+//                                    .tint(.red)
+//                            })
+//                        }
                 }
             }
             .navigationTitle("To Do")
             .toolbar(content: {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {}, label: {
+                    Button(action: {
+                        isShowingAddNewTaskView.toggle()
+                    }, label: {
                         Image(systemName: "plus")
                     })
                 }
             })
         }
+//        .fullScreenCover(isPresented: $isShowingAddNewTaskView, content: {
+//            NavigationStack{
+//                AddTaskView()
+        //            }
+        //        })
+        .sheet(isPresented: $isShowingAddNewTaskView) {
+            AddTaskView(tasks: $tasks,
+                        isShowingAddNewTaskView: $isShowingAddNewTaskView)
+        }
+        
     }
 }
 
